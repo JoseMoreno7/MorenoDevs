cf3ec8def20cc30d5fd026fde2e992ae27bfeb25
jose <jose7arevalo7moreno7@gmail.com>
2025-05-09 13:39:24 -0500

    cambiando el nombre de la pagina en el navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
             <div className="flex justify-between items-center h-16">
                 <a href="#home" className="font-mono text-xl font-bold text-white">
                     {" "}
-                    moreno<span className="text-blue-500">.tech</span>{" "}
+                    moreno<span className="text-blue-500">.devs</span>{" "}
                 </a>
 
                 {!menuOpen && (
